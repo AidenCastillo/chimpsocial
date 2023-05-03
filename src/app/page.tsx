0c69b68f58f56bd2ee723a11dc686b9da8e7cd1a
AidenCastillo <amc-mcjarjar@hotmail.com>
2023-05-02 23:54:29 -0500

    created login page grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import styles from  './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <div className={styles.description}>
-        <p>
-          Welcome to ChimpSocial!
-        </p>
-     </div>
+      <div className={styles.float_container}>
+        <div className={styles.float_child}>
+          <div className={styles.logo}>
+            <img src="/logo.png" alt="logo" />
+          </div>
+        </div>
+        <div className={styles.float_child}>
+          <div className={styles.title}>
+            <p>Happening Now</p>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
