bd405e95b105fba5a625abbf2ea2bc6c16707a05
AidenCastillo <amc-mcjarjar@hotmail.com>
2023-05-03 23:57:19 -0500

    created new url pathing

---
+++ src/app/(client)/home/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+        <main>
+            <div className="columns">
+                <div className="left">
+                    <p>left</p>
+                </div>
+                <div className="center">
+                    <p>center</p>
+                </div>
+                <div className="right">
+                    <p>right</p>
+                </div>
+            </div>
+        </main>
+    )
+}
