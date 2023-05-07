52bfe161a33e9e595748fbd2e6d17e6b8f48380a
AidenCastillo <amc-mcjarjar@hotmail.com>
2023-05-06 23:48:03 -0500

    navigation bar fix

--- src/app/(client)/navigationbar.tsx
+++ src/app/(client)/navigationbar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import logo from "../../../public/next.svg";
+import logo from "../../../public/logo.png";
 import homeIcon from "../../../public/home.svg";
 import exploreIcon from "../../../public/search.svg";
 import messageIcon from "../../../public/message-square.svg";
 import listIcon from "../../../public/list.svg";
 import profileIcon from "../../../public/user.svg";
+import notificationIcon from "../../../public/bell.svg";
+import bookmarkIcon from "../../../public/bookmark.svg";
+import moreIcon from "../../../public/more-horizontal.svg";
 import styles from "./navigationbar.module.css";
 import Link from "next/link";
 // import { Link } from "react-router-dom";
@@ -16,24 +19,6 @@ type Props = {
 };
 
 export default function NavigationButton({ version }: Props) {
-  // var link = "test";
-  // var image = "test";
-  // if (version == "home") {
-  //   link = "/home";
-  //   image = homeImage;
-  // } else if (version == "explore") {
-  //   link = "/explore";
-  //   image = exploreImage;
-  // } else if (version == "messages") {
-  //   link = "/messages";
-  //   image = messageImage;
-  // } else if (version == "lists") {
-  //   link = "/lists";
-  //   image = listImage;
-  // } else if (version == "profile") {
-  //   link = "/profile";
-  //   image = profileImage;
-  // }
 
   return (
     
@@ -58,14 +43,14 @@ export default function NavigationButton({ version }: Props) {
             </button>
             </Link>
           </div>
-          {/* <div className={styles.notificationsButton}>
-            <Link to="/notifications">
+          <div className={styles.notificationsButton}>
+            <Link href="/notifications">
             <button>
               <Image src={notificationIcon} alt="Notifications" />
               Notifications
             </button>
             </Link>
-          </div> */}
+          </div>
           <div className={styles.messagesButton}>
             <Link href="/messages">
             <button>
@@ -74,14 +59,14 @@ export default function NavigationButton({ version }: Props) {
             </button>
             </Link>
           </div>
-          {/* <div className={styles.bookmarksButton}>
-            <Link to="/bookmarks">
+          <div className={styles.bookmarksButton}>
+            <Link href="/bookmarks">
             <button>
               <Image src={bookmarkIcon} alt="Bookmarks" />
               Bookmarks
             </button>
             </Link>
-          </div> */}
+          </div>
           <div className={styles.listsButton}>
             <Link href="/lists">
             <button>
@@ -98,12 +83,12 @@ export default function NavigationButton({ version }: Props) {
             </button>
             </Link>
           </div>
-          {/* <div className={styles.moreButton}>
+          <div className={styles.moreButton}>
             <button>
               <Image src={moreIcon} alt="More" />
               More
             </button>
-          </div> */}
+          </div>
           <div className={styles.tweetButton}>
             <button>Tweet</button>
           </div>
