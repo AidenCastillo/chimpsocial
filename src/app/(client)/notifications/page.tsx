c8e5a267c426dd536dbf7a6982471f3a247eea18
AidenCastillo <amc-mcjarjar@hotmail.com>
2023-05-23 22:57:06 -0500

    notifications header

--- src/app/(client)/notifications/page.tsx
+++ src/app/(client)/notifications/page.tsx
@@ -1,7 +1,21 @@
+import styles from "./page.module.css";
 export default function Notification() {
 		return (
 				<div>
+					<div className={styles.header}>
 						<h1>Notifications</h1>
+					</div>
+					<div className={styles.categories}>
+						<div className={styles.All}>
+						<h2>All</h2>
+						</div>
+						<div className={styles.Verified}>
+							<h2>Verified</h2>
+						</div>
+						<div className={styles.Mentions}>
+							<h2>Mentions</h2>
+						</div>
+					</div>
 				</div>
 		)
 }
